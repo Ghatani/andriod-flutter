3d65ecc440955307b2933d51cd7f25c6b67d7043
Santosh Ghatani <santght90@gmail.com>
2022-02-22 01:50:34 +0545

    registerUpdate

--- lib/screen/register.dart
+++ lib/screen/register.dart
@@ -1,1 +1,113 @@
 import 'package:flutter/material.dart';
+
+class register extends StatefulWidget {
+  const register({ Key? key }) : super(key: key);
+
+  @override
+  _registerState createState() => _registerState();
+}
+
+class _registerState extends State<register> {
+  
+  @override
+  Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16),
+        child: ListView(
+          children: [
+            SizedBox(height: screenHeight * .12),
+            const Text(
+              "Create Account,",
+              style: TextStyle(
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: screenHeight * .01),
+            Text(
+              "Sign up to get started!",
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black.withOpacity(.6),
+              ),
+            ),
+            SizedBox(height: screenHeight * .12),
+            TextFormField(
+              onChanged: (value){
+
+              },
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.email),
+                labelText: 'Email',
+                hintText: 'Enter your email',
+                border: OutlineInputBorder(),
+              ),
+              keyboardType: TextInputType.emailAddress,
+              textInputAction: TextInputAction.next,
+              autofocus: true,
+            ),
+            SizedBox(height: screenHeight * .025),
+            TextFormField(
+              onChanged: (value) {
+                
+              },
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                labelText: 'Password',
+                hintText: 'Enter your password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+              textInputAction: TextInputAction.next,
+            ),
+            SizedBox(height: screenHeight * .025),
+            TextFormField(
+              onChanged: (value) {
+                
+              },
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.lock),
+                labelText: 'Confirm Password',
+                hintText: 'Enter your password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+              textInputAction: TextInputAction.done,
+            ),
+            SizedBox(
+              height: screenHeight * .075,
+            ),
+            ElevatedButton(
+              
+              onPressed: (){},
+              child: Text("Sign Up")
+            ),
+            SizedBox(
+              height: screenHeight * .125,
+            ),
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: RichText(
+                text: const TextSpan(
+                  text: "I'm already a member, ",
+                  style: TextStyle(color: Colors.black),
+                  children: [
+                    TextSpan(
+                      text: "Sign In",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
