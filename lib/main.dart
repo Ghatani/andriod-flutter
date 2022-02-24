f36801616d3def6e27597c1cf750338e96cdbce9
Santosh Ghatani <santght90@gmail.com>
2022-02-24 14:46:49 +0545

    RouteAdded

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'package:dolakha_supplier_system/screen/addRecord.dart';
+import 'package:dolakha_supplier_system/screen/addTransaction.dart';
+import 'package:dolakha_supplier_system/screen/customers.dart';
 import 'package:dolakha_supplier_system/screen/homePage.dart';
 import 'package:dolakha_supplier_system/screen/login.dart';
+import 'package:dolakha_supplier_system/screen/materials.dart';
+import 'package:dolakha_supplier_system/screen/profile.dart';
 import 'package:dolakha_supplier_system/screen/register.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +21,11 @@ class myApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/':(context) => login(),
-        '/register' : (context) => register()
+        '/register' : (context) => register(),
+        '/home' : (context) => profile(),
+        '/customer' : (context) => customer(),
+        '/addRecord' : (context) => addRecord(),
+        '/addTransaction' : (context) => addTransaction(),
       },
     );
   }
